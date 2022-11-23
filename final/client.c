f58e4c9975a6747cbddabec25d2b2f4a946d4d28
Chris A <1drturtle@gmail.com>
2022-11-22 21:17:17 -0500

    big final progress

--- final/client.c
+++ final/client.c
@@ -4,79 +4,115 @@
 
 #include <stdio.h>
 #include <winsock2.h>
+#include <stdlib.h>
 
 #pragma comment(lib, "ws2_32.lib") // Winsock Library
 
+#define HOST "127.0.0.1"
+#define PORT 8888
+
 int main(int argc, char *argv[])
 {
+    printf("--- Connecting ...");
+    // trig type (binary, but takes integer)
+    // sin:  1  cos: 2  tan: 3
+    // asin: 4 acos: 5 atan: 6
 
-    if (argc < 2)
-    {
-        puts("specify number to send to server");
-        return -1;
-    }
-    int user_input = atoi(argv[1]);
+    double user_angle;
+    char trig_type_buf[10];
+    int trig_type;
 
+    // library
     WSADATA wsa;
-    SOCKET s;
+    // client socket
+    SOCKET client_sock;
+    // server information
     struct sockaddr_in server;
-    char *message, server_reply[2000];
-    char *out_buffer = calloc(256, sizeof(char));
-    int recv_size;
 
-    printf("\nInitialising Winsock...");
+    // Set up connection
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
         printf("Failed. Error Code : %d", WSAGetLastError());
         return 1;
     }
-
-    printf("Initialised.\n");
+    printf("Lib✓");
 
     // Create a socket
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    // socket(AF_INET, SOCK_STREAM, 0)
+    // AF_INET -> IPV4
+    // SOCK_STREAM -> TCP
+    // 0 -> No extra flags
+    if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
     {
-        printf("Could not create socket : %d", WSAGetLastError());
+        printf("Could not create socket : %d\n", WSAGetLastError());
     }
+    printf("Sock✓");
 
-    printf("Socket created.\n");
-
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // populate server information
+    // host is static, and inet_addr converts it to a machine-usable internet address
+    server.sin_addr.s_addr = inet_addr(HOST);
+    // AF_INET -> IPV4
     server.sin_family = AF_INET;
-    server.sin_port = htons(8888);
+    // htons -> converts port to network byte order for TCP
+    server.sin_port = htons(PORT);
 
     // Connect to remote server
-    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    if (connect(client_sock, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
-        puts("connect error");
+        puts("Connection error.");
         return 1;
     }
 
-    puts("Connected");
+    printf("Bind✓\n");
 
-    // Send angle from argument
-    printf("user in: %d\n", user_input);
-    int converted_in = htonl(user_input);
+    // connected. driver code above
 
     int send_status;
-
-    send_status = send(s, &converted_in, sizeof(converted_in), 0);
-    if (send_status < 0)
+    double result = 0;
+    puts("Connected. Starting program");
+    while (1)
     {
-        puts("Send failed");
-        return 1;
+        // fetch user arguments
+        printf("Enter Radian Angle to Convert > ");
+        scanf("%lf", &user_angle);
+
+        printf("Enter Math to perform. Sin=1, Cos=2, Tan=3. (add 3 for inverse, 9 for quit) > ");
+        scanf("%d", &trig_type);
+
+        if (trig_type == 0)
+        {
+            puts("\nInvalid Angle or Type performed, looping.");
+            continue;
+        }
+        else if (trig_type == 9)
+        {
+            puts("Quitting...");
+            break;
+        }
+
+        // step one: send angle & trig type
+        // - Send angle
+        if (send(client_sock, (char *)&user_angle, sizeof(user_angle), 0) < 0)
+        {
+            puts("Server connection broken, quitting.");
+            break;
+        }
+        // - Send type
+        if (send(client_sock, (char *)&trig_type, sizeof(trig_type), 0) < 0)
+        {
+            puts("Server connection broken, quitting.");
+            break;
+        }
+        // step two: receive result as double radians and print
+        if (recv(client_sock, (char *)&result, sizeof(result), 0) == SOCKET_ERROR)
+        {
+            puts("Server connection broken, quitting.");
+            break;
+        }
+        printf("result: %lf rads\n", result);
+        // step three: loop! 🔁🔁
+        puts("\nAgain!");
     }
-    puts("Data Send\n");
-
-    // Receive a reply from the server
-    if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR)
-    {
-        puts("recv failed");
-    }
-
-    puts("Reply received\n");
-
-    puts(server_reply);
-
+    closesocket(client_sock);
     return 0;
 }
--- final/server.c
+++ final/server.c
@@ -5,8 +5,12 @@
 #include <string.h>
 // structs needed to create sockets
 #include <winsock2.h>
+// math!
+#include <math.h>
 
 #pragma comment(lib, "ws2_32.lib") // Winsock Library
+
+void client_handler(SOCKET sock);
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +21,7 @@ int main(int argc, char *argv[])
     // we have to initialize the library. it's weird
     WSADATA wsa;
 
-    puts("\nInitialising Winsock...");
+    printf("Starting...");
 
     // we are giving the WSAStartup function the memory address of the struct and the version(?) of the library to load
     // it returns 0 if successful, so we stop if not 0
@@ -26,37 +30,39 @@ int main(int argc, char *argv[])
         printf("Failed. Error Code : %d\n", WSAGetLastError());
         return 1;
     }
-    puts("Initialised.");
+    printf("Lib✅");
 
     // make a struct to store our (server) information
     struct sockaddr_in server;
-
-    // now we actually want a socket
-    SOCKET sock_p;
-    sock_p = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_p == INVALID_SOCKET)
-    {
-        printf("Could not create socket : %d\n", WSAGetLastError());
-        return -1;
-    }
-
-    puts("Socket created.\n");
 
     // Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(8888);
 
+    // now we actually want a socket
+    // a socket is just an integer that points to a file descriptor on the operating system
+    // a reference number
+    SOCKET server_sock;
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d\n", WSAGetLastError());
+        return -1;
+    }
+
+    printf("Sock");
+
     // bind the socket (reserves the port for this process)
-    if (bind(sock_p, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+    if (bind(server_sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
     {
         printf("Bind failed with error code : %d", WSAGetLastError());
         exit(EXIT_FAILURE);
     }
-    puts("Socked bound");
+    printf("Bind✅\n");
 
     // Listen to incoming connections
-    listen(sock_p, 3);
+    listen(server_sock, 3);
 
     // Accept and incoming connection
     puts("Waiting for incoming connections...");
@@ -67,38 +73,99 @@ int main(int argc, char *argv[])
     int addr_len = sizeof(struct sockaddr_in);
 
     int received_int, client_input;
+    double to_receive;
 
-    while ((new_socket = accept(sock_p, (struct sockaddr *)&client, &addr_len)) != INVALID_SOCKET)
+    while ((new_socket = accept(server_sock, (struct sockaddr *)&client, &addr_len)) != INVALID_SOCKET)
     {
-        received_int = 0;
-        puts("Connection accepted");
-
-        // receive number from client
-        recv(new_socket, &received_int, sizeof(received_int), 0);
-        client_input = ntohl(received_int);
-
-        // print out what we got
-        printf("%d\n", client_input);
-
-        // Reply to the client
-        puts("Replying...");
-
-        serv_msg = "server message out\n";
-        send(new_socket, serv_msg, strlen(serv_msg), 0);
-
-        puts("Waiting for next client...");
+        client_handler(new_socket);
     }
 
     if (new_socket == INVALID_SOCKET)
     {
-        printf("accept failed with error code : %d", WSAGetLastError());
+        // cause of this is usually messing with internet settings
+        printf("Accept failed with error code : %d", WSAGetLastError());
         return 1;
     }
 
     puts("Server stopping...");
 
     // clean up the library
-    closesocket(sock_p);
-    closesocket(new_socket);
+    closesocket(server_sock);
     WSACleanup();
+}
+void client_handler(SOCKET sock)
+{
+    double client_radian = 0;
+    int trig_type = 0;
+    double result = 0;
+
+    puts("Connection accepted");
+
+    while (1)
+    {
+        puts("Running Calculation...");
+        // step one recv trig function of choice
+
+        // receive number from client
+        // recv (socket, buff, size, flags)
+        // socket -> connection
+        // buff -> where to write data. default is (char *) but can be anything if you are *Sure* of types
+        // size -> how big
+        // flags -> special connection flags
+        // positive number good
+        // <=0 Bad
+        if (recv(sock, (char *)&client_radian, sizeof(client_radian), 0) == SOCKET_ERROR)
+        {
+            break;
+        }
+        // step two recv double representing radian value
+        if (recv(sock, (char *)&trig_type, sizeof(trig_type), 0) == SOCKET_ERROR)
+        {
+            break;
+        }
+
+        // match input with appropriate trig functions
+        // all trig functions take and return double radians
+        switch (trig_type)
+        {
+        case 1:
+            result = sin(client_radian);
+            break;
+
+        case 2:
+            result = cos(client_radian);
+            break;
+
+        case 3:
+            result = tan(client_radian);
+            break;
+
+        case 4:
+            result = asin(client_radian);
+            break;
+
+        case 5:
+            result = acos(client_radian);
+            break;
+
+        case 6:
+            result = atan(client_radian);
+            break;
+        }
+
+        // Reply to the client
+        puts("Replying...");
+
+        // send (socket, buff, size, flags)
+        // similar to recv
+        if (send(sock, (char *)&result, sizeof(result), 0) < 0)
+        {
+            break;
+        }
+        puts("🔁");
+    }
+
+    puts("Connection closed");
+    closesocket(sock);
+    puts("Waiting for next client...\n");
 }
